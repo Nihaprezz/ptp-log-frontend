6b230d587c7be0a2fd227d3ed4ecb516ced8c5d1
nihaprezz <serg13prez@outlook.com>
2020-03-14 20:49:00 -0400

    view results button correctly showing when results come back

--- src/skiptracepage/SkipRecords.js
+++ src/skiptracepage/SkipRecords.js
@@ -2,6 +2,7 @@ import React from "react"
 import { Link } from "react-router-dom"
 
 const SkipRecords = (props) => {
+    console.log(props)
     let { id, acct_no, first_name, last_name, data_created } = props.skipObj
     let cuName = props.skipObj.creditunion.name
 
@@ -12,8 +13,8 @@ const SkipRecords = (props) => {
             <td>{first_name}</td>
             <td>{last_name}</td>
             <td>{data_created}</td>
-            {props.skipObj.results =! "pending" ? (
-                <Link className="ui primary button" to={`/skip/${id}`}> View Results </Link>
+            {props.skipObj.results !== "pending" ? (
+                <td><Link className="ui primary button" to={`/skip/${id}`}> View Results </Link></td>
             ): null}
         </tr>
     )   
