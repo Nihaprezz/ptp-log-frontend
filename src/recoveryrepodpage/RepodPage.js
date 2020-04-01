6e4b7990f5f78ad09fdab46340664a3d7b8a8f46
nihaprezz <serg13prez@outlook.com>
2020-04-01 10:42:06 -0400

    toggle between all repod and user repod records working

--- src/recoveryrepodpage/RepodPage.js
+++ src/recoveryrepodpage/RepodPage.js
@@ -9,11 +9,36 @@ class RepodPage extends React.Component {
         super();
 
         this.state = {
-            repoRecords: []
+            repoRecords: [],
+            showAll: false,
         }
     }
 
     componentDidMount(){
+        if(this.props.user.isadmin){
+            this.fetchUserRepos()
+        } else {
+            this.fetchAllRepos()
+        }
+    }
+
+    fetchUserRepos = () => {
+        let id = this.props.user.id
+        fetch(backend_url + `/repo_orders/active/all_repod/${id}`, {
+            headers: {
+                "Authorization" : `Bearer ${localStorage.getItem('jwt')}`,
+                "Content-Type": 'application/json',
+                "Accept": 'application/json'
+            }
+        })
+        .then(resp => resp.json())
+        .then(myRepodVehs => {
+            this.setState({repoRecords: myRepodVehs})
+        })
+        .catch(err => alert(err))
+    }
+
+    fetchAllRepos = () => {
         fetch(backend_url + '/repo_orders/active/all_repod', {
             headers: {
                 "Authorization" : `Bearer ${localStorage.getItem('jwt')}`,
@@ -28,16 +53,33 @@ class RepodPage extends React.Component {
         .catch(err => alert(err))
     }
 
+    toggleShowAll = () => {
+       this.setState({showAll: !this.state.showAll}, () => {
+           if(!this.state.showAll){
+               this.fetchUserRepos()
+           } else {
+               this.fetchAllRepos()
+           }
+       })
+    }
+
     render(){
         let { isadmin } = this.props.user;
-
+        let btnText = this.state.showAll ? 'Show Your Repos' : 'Show All'
+        
         return (
             <div>
                 <h1 style={{textAlign: 'left', width: '98%', margin: 'auto'}} className="ui dividing header">
                         Repossessed Vehicles
                 </h1>  
-       
-                <p>Showing Records for : {this.props.user.username}</p>
+                
+                <div style={{textAlign: 'left', padding: '1%'}}>
+                    {isadmin ? (
+                            <button className="ui secondary button" onClick={() => this.toggleShowAll()}>
+                                {btnText}
+                            </button>
+                    ) : null}
+                </div>
 
                 < RepodFilterBar />
 
