91066c2fc8b26b2d458d85e45ae128ec5c19d5be
nihaprezz <serg13prez@outlook.com>
2020-04-07 12:38:52 -0400

    sold info header done

--- src/soldshowpage/components/SoldInfoHeader.js
+++ src/soldshowpage/components/SoldInfoHeader.js
@@ -1,11 +1,12 @@
 import React from "react";
 
 const SoldInfoHeader = (props) => {
-    let {acct_no, first_name, last_name, repod_on, repo_company } = props.soldRecord;
+    let {acct_no, first_name, last_name, repod_on, repo_company, veh_info, veh_vin } = props.soldRecord;
     let {sale_location, sold_amt} = props.soldRecord.sold_record;
+    let { sale_date } = props.soldRecord.auction_record
     let name;
     props.soldRecord.creditunion ? name = props.soldRecord.creditunion.name : name = "CU Deleted"
-    console.log(props)
+
     return (
         <div className="repo-order-details-cont">
             <div>
@@ -13,12 +14,15 @@ const SoldInfoHeader = (props) => {
                 <p>Credit Union: {name}</p>
                 <p>Member Name : {`${first_name} ${last_name}`}</p>
                 <p>Transport Date: {repod_on}</p>
+                <p>Vehicle: {veh_info}</p>
             </div>
 
             <div>
                 <p>Repo Company: {repo_company}</p>
                 <p>Sale Location: {sale_location}</p>
-                <p>Sold Amt: {sold_amt}</p>
+                <p>Sale Date: {sale_date}</p>
+                <p>Sold Amt: ${sold_amt.toLocaleString()}</p>
+                <p>Vin: {veh_vin}</p>
             </div>
         </div>   
     )
